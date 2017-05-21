1086970bd5e03e9a610a7b4c5e08711b100bf810
Kimberly Strumpf <kimberly_lara.strumpf@smail.th-koeln.de>
2017-05-21 15:15:29 +0200

    Aufgabenteil 3 - sortieren

--- Aufgabe01/Aufgabe01.js
+++ Aufgabe01/Aufgabe01.js
@@ -10,8 +10,31 @@ for (x in obj.cities){
   console.log("name: " + chalk.green(obj.cities[x].name));
   console.log("country: " + chalk.yellow(obj.cities[x].country));
   console.log("population: " + chalk.blue(obj.cities[x].population));
-  console.log("--------------------");
+  console.log("--------------------"); };
 
-};
+  obj.cities.sort(function(a ,b){
+    if(a.population>b.population){
+        return -1;
+      }
+      else if(a.population<b.population){
+        return 1;
+      }
+      else{
+        return 0;
+      }
+    });
+
+  var x = JSON.stringify(obj);
+
+  fs.writeFile(__dirname + "/staedte_sortiert.json", x, function(err)  {
+    if (err) throw err;
+      });
+      console.log('\n\nThe file has been saved!');
+      console.log('\n\nNeue Reihenfolge\n\n');
+      for (x in obj.cities){
+        console.log("name: " + chalk.green(obj.cities[x].name));
+        console.log("country: " + chalk.yellow(obj.cities[x].country));
+        console.log("population: " + chalk.blue(obj.cities[x].population));
+        console.log("--------------------"); };
 
 });
